fff32ef0c9f4f09afd96493d07b8f270dbe5eb5b
Keira Dueck <sylae@calref.net>
2023-02-12 11:29:29 -0800

    initial commit for testing

---
+++ process.ps1
@@ -0,0 +1,13 @@
+Get-ChildItem "1x1" -Filter *.png | Foreach-Object -Parallel {
+  optipng $_.FullName
+} -ThrottleLimit 8
+
+Get-ChildItem "1x2" -Filter *.png | Foreach-Object -Parallel {
+  optipng $_.FullName
+} -ThrottleLimit 8
+
+Get-ChildItem "1x4" -Filter *.png | Foreach-Object -Parallel {
+  optipng $_.FullName
+} -ThrottleLimit 8
+
+php generateLocators.php
