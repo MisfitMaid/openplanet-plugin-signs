75c84bb5444999c94f204951a0792b99f253d35d
Keira Dueck <sylae@calref.net>
2023-02-12 11:37:29 -0800

    .loc generator script

---
+++ generateLocators.php
@@ -0,0 +1,14 @@
+<?php
+
+function genLoc(string $dir): void { // shame about season two
+	foreach(glob($dir . "/*") as $file) {
+		if (str_ends_with($file, ".loc")) continue;
+		
+		$path = "https://misfitmaid.github.io/openplanet-plugin-signs/$file";
+		file_put_contents($file . ".loc", $path);
+	}
+}
+
+genLoc("1x1");
+genLoc("1x2");
+genLoc("1x4");
