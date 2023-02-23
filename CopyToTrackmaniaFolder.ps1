eb33f8631b54314176e88661f9dd66d00fc480eb
Keira Dueck <sylae@calref.net>
2023-02-22 18:13:53 -0800

    commented out junction idea

--- CopyToTrackmaniaFolder.ps1
+++ CopyToTrackmaniaFolder.ps1
@@ -7,3 +7,8 @@ $skinny = $TM + "Advertisement4x1\OpenplanetPlugins"
 Copy-Item -Recurse "1x1/" $square
 Copy-Item -Recurse "1x2/" $fat
 Copy-Item -Recurse "1x4/" $skinny
+
+# alternative idea...
+# junction $square 1x1
+# junction $fat 1x2
+# junction $skinny 1x4
